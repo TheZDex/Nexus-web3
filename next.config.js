db4b276d3ecca5e4da95856f43619944c5a23272
nexus <nexus@zdex.one>
2023-09-06 22:36:35 +0800

    expose env variable

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    WALLETCONNECT_PROJECT_ID: process.env.WALLETCONNECT_PROJECT_ID,
+  }
 }
 
 module.exports = nextConfig
